5aa541b7bd33b4d62da8a9a98865ae3da237f4a5
felixtram <U315546@L76BT3G72.AD001.InfoFTPS.COM>
2017-03-16 10:40:55 -0600

    added functionality to download css, added jsdocs style messages

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,140 @@
-function changeCss(customCss){
-	//replace current style with customCss string
-	$("<style>" + customCss +"</style>").replaceAll("style");
-}
+/**
+ * @file script.js
+ * Functions for eProtect iFrame advanced demo site
+ */
 
-function init(){
-	//add tabbing functionality to text area
-	$("textarea").keydown(function(e) {
-    if(e.keyCode === 9) { // tab was pressed
-        // get caret position/selection
-        var start = this.selectionStart;
-        var end = this.selectionEnd;
+/**
+ * Init event handlers and textarea
+ * @function init
+ */
+function init() {
+  //add tabbing functionality to text area
+  allowTabsInTextArea();
 
-        var $this = $(this);
-        var value = $this.val();
+  //create codemirror to add syntzx highlighting
+  var editor = CodeMirror.fromTextArea(document.getElementById("customCss"), {
+    theme: "night",
+    tabSize: 2
+  });
 
-        // set textarea value to: text before caret + tab + text after caret
-        $this.val(value.substring(0, start)
-                    + "/t"
-                    + value.substring(end));
+  //add event handler to change css on button click
+  $("#changeCss").on("click", function() {
+    var customCss = editor.getValue();
 
-        // put caret at right position again (add one for the tab)
-        this.selectionStart = this.selectionEnd = start + 1;
+    //transition and change css
+    $("#payForm").fadeTo(750, 0, function() {
+      changeCss(customCss);
+    });
 
-        // prevent the focus lose
-        e.preventDefault();
-    }
-	});
+    $("#payForm").fadeTo(750, 1);
+  })
 
-	//add syntax hightlighting to textarea
-      var editor = CodeMirror.fromTextArea( document.getElementById("customCss"), {
-        theme: "night",
-        tabSize: 2
-      });
+  //add event handler to download css on button click 
+  $("#downloadCss").on("click", function() {
+    downloadInnerHtml("test.css", 'text');
+  })
 
-	//add event handler to button
-	$("#changeCss").on("click", function(){
-		var customCss = editor.getValue();
-
-        //transition and change css
-        $("#payForm").fadeTo(750, 0, function(){
-            changeCss( customCss );    
-        });
-        
-        $("#payForm").fadeTo(750, 1);
-	})	
-
-	//remove loading div
-	$( document ).ready(function() {
-    $(".loading-wrap").fadeOut(2000, function() { $(this).remove(); });
+  //remove loading div
+  $(document).ready(function() {
+    $(".loading-wrap").fadeOut(2000, function() {
+      $(this).remove();
+    });
   })
 }
 
+/**
+ * Add tabbing functionality to textareas
+ * @function allowTabsInTextArea
+ */
+function allowTabsInTextArea(){
+  //add tabbing functionality to text area
+  $("textarea").keydown(function(e) {
+    if (e.keyCode === 9) { // tab was pressed
+      // get caret position/selection
+      var start = this.selectionStart;
+      var end = this.selectionEnd;
 
+      var $this = $(this);
+      var value = $this.val();
 
-window.changeCss = changeCss;
+      // set textarea value to: text before caret + tab + text after caret
+      $this.val(value.substring(0, start) +
+        "/t" +
+        value.substring(end));
+
+      // put caret at right position again (add one for the tab)
+      this.selectionStart = this.selectionEnd = start + 1;
+
+      // prevent the focus lose
+      e.preventDefault();
+    }
+  });
+}
+
+/**
+ * Change css of form to string passed in
+ * @function changeCss
+ * @param {String} newCss
+ */
+function changeCss(newCss) {
+  //replace current style with customCss string
+  $("<style>" + newCss + "</style>").replaceAll("style");
+}
+
+/**
+ * Allow users to download css from textarea
+ * @function downloadInnerHtml
+ * @param {String} filename
+ * @param {String} mimeType
+ */
+function downloadInnerHtml(filename, mimeType) {
+  var editor = $('.CodeMirror')[0].CodeMirror;
+
+  var elHtml = editor.getValue();
+  download(elHtml, "test.css", "text/css");
+}
+
+/**
+ * Download functionality
+ * @function download
+ * @param {String} strData
+ * @param {String} strFileName
+ * @param {String} strMimeType
+ * @returns {Boolean}
+ */
+function download(strData, strFileName, strMimeType) {
+  var D = document,
+    a = D.createElement("a");
+  strMimeType = strMimeType || "application/octet-stream";
+  
+  
+  if (navigator.msSaveBlob) { // IE10
+    return navigator.msSaveBlob(new Blob([strData], {
+      type: strMimeType
+    }), strFileName);
+  } /* end if(navigator.msSaveBlob) */
+  
+  
+  if ('download' in a) { //html5 A[download]
+    a.href = "data:" + strMimeType + "," + encodeURIComponent(strData);
+    a.setAttribute("download", strFileName);
+    a.innerHTML = "downloading...";
+    D.body.appendChild(a);
+    setTimeout(function() {
+      a.click();
+      D.body.removeChild(a);
+    }, 66);
+    return true;
+  } /* end if('download' in a) */
+  
+  
+  //do iframe dataURL download (old ch+FF):
+  var f = D.createElement("iframe");
+  D.body.appendChild(f);
+  f.src = "data:" + strMimeType + "," + encodeURIComponent(strData);
+  
+  setTimeout(function() {
+    D.body.removeChild(f);
+  }, 333);
+  return true;
+} /* end download() */
