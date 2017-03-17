07a61e268ed5ecf893389bedc4cf8af1db9fcf06
felixtram <U315546@L76BT3G72.AD001.InfoFTPS.COM>
2017-03-17 16:07:08 -0600

    changed viewport settings

--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,6 @@
  * @function init
  */
 function init() {
-  //add tabbing functionality to text area
-  allowTabsInTextArea();
 
   //create codemirror to add syntzx highlighting
   //var editor = CodeMirror.fromTextArea(document.getElementById("customCss"), {
@@ -21,6 +19,7 @@ function init() {
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/css");
     editor.setShowPrintMargin(false);
+    editor.renderer.setScrollMargin(10, 10)
   //add event handler to change css on button click
   $("#changeCss").on("click", function() {
     var editor = ace.edit("editor");
@@ -45,35 +44,6 @@ function init() {
       $(this).remove();
     });
   })
-}
-
-/**
- * Add tabbing functionality to textareas
- * @function allowTabsInTextArea
- */
-function allowTabsInTextArea(){
-  //add tabbing functionality to text area
-  $("textarea").keydown(function(e) {
-    if (e.keyCode === 9) { // tab was pressed
-      // get caret position/selection
-      var start = this.selectionStart;
-      var end = this.selectionEnd;
-
-      var $this = $(this);
-      var value = $this.val();
-
-      // set textarea value to: text before caret + tab + text after caret
-      $this.val(value.substring(0, start) +
-        "/t" +
-        value.substring(end));
-
-      // put caret at right position again (add one for the tab)
-      this.selectionStart = this.selectionEnd = start + 1;
-
-      // prevent the focus lose
-      e.preventDefault();
-    }
-  });
 }
 
 /**
