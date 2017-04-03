08944c9eeb997916d76a10fab216d3fadb3e0d53
felixtram <U315546@L76BT3G72.AD001.InfoFTPS.COM>
2017-04-03 10:28:12 -0600

    changed cvc to cvv and accountnumber to accountNumber

--- scripts/form/validation-listeners.js
+++ scripts/form/validation-listeners.js
@@ -4,12 +4,28 @@
  */
 
 //Account Number Events
-$('#accountnumber').blur(panBlur);
-$('#accountnumber').bind("paste", panPaste);
-$("#accountnumber").keypress(panKeyPress);
-$("#accountnumber").keyup(panKeyUp);
+$('#accountNumber').blur(panBlur);
+$('#accountNumber').bind("paste", panPaste);
+$("#accountNumber").keypress(panKeyPress);
+$("#accountNumber").keyup(panKeyUp);
 
 // CVC Events
-$('#cvc').blur(cvcBlur)
-$('#cvc').bind("paste", cvcPaste);
-$("#cvc").keypress(cvcKeyPress);
+$('#cvv').blur(cvcBlur)
+$('#cvv').bind("paste", cvcPaste);
+$("#cvv").keypress(cvcKeyPress);
+
+// Expiration Events
+$('#expMonth').blur(handleMonthEvent);
+$('#expMonth').change(handleMonthEvent);
+$('#expYear').blur(handleYearEvent);
+$('#expYear').change(handleYearEvent);
+
+//clear success and errors on focus
+$("#cvv").focus( function(e){
+	var elements = $('#cvvDiv, #cvv,#cvvLabelBefore,#cvvNumberLabelAfter,#cvvLabelText')
+	elements.removeClass();
+});
+$("#accountNumber").focus( function(e){
+	var elements = $('#numberDiv,#accountNumber,#accountNumberLabelBefore,#accountNumberLabelAfter,#accountNumberLabelText')
+	elements.removeClass();
+});
